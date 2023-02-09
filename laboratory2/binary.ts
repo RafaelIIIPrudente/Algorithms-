2235d4b97c6b20167e86725030cda8db2d2f8b5d
RafaelIIIPrudente <johndoe@example.comrafaeliii.prudente-21@cpu.edu.ph>
2023-02-09 14:13:29 +0800

    lab 2

--- laboratory2/binary.ts
+++ laboratory2/binary.ts
@@ -1,19 +1,23 @@
-export default function binarySearch(array: number[], target:number): number {
-  let leftmost: number = 0; 
-  let righmost: number = array.length - 1; 
+export default function binarySearch(array: number[], target: number): number {
+  let leftmost: number = 0;
+  let righmost: number = array.length - 1;
+
 
   while (leftmost <= righmost) {
     const middle: number = Math.floor((leftmost + righmost) / 2);
-    if (array[middle] === target) {
+
+    if (array[middle] < target) {
       leftmost = middle + 1;
-    } else if(array[middle] > target) {
-      middle - 1;
-    } else {
+    }
+    else if (array[middle] > target) {
+      righmost = middle - 1;
+    }
+    else {
       return middle;
     }
   }
-    return - 1;
+  return - 1;
 }
 
-let try1 = binarySearch([1,2,3,4,5,6,7,8], 2)
+let try1 = binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10)
 console.log(try1)
